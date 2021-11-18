e6236419a8fd6f369429dc5d6daf79aeeb817b4b
Fillipe Meireles <hebriquefillipe@gmail.com>
2021-11-18 15:48:22 -0300

    model reference translated

--- src/models/Sessions.ts
+++ src/models/Sessions.ts
@@ -8,17 +8,17 @@ export default class Sessions extends MasterModel {
   @Column({ nullable: false, unique: true })
   session_id: string;
 
-  @ManyToOne(()=> Psicologo, (psicologo) => psicologo.id )
+  @ManyToOne(() => Psicologo, (psicologo) => psicologo.id)
   psychologist: Psicologo;
 
-  @ManyToOne(()=> Paciente, (paciente) => paciente.id )
+  @ManyToOne(() => Paciente, (paciente) => paciente.id)
   patient: Paciente;
 
   @Column()
-  patient_socket_id: string;
+  paciente_socket_id: string;
 
   @Column()
-  psychologist_socket_id: string;
+  psicologo_socket_id: string;
 
   @Column({ nullable: false, unique: true })
   session_link: string;
