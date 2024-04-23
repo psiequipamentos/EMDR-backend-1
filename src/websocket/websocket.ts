49fabb2f460c6c9836c73c7716db800f779b78fc
Joao Vitor <90356472+Raulens@users.noreply.github.com>
2024-04-23 14:54:10 -0300

    Update websocket.ts

--- src/websocket/websocket.ts
+++ src/websocket/websocket.ts
@@ -12,7 +12,7 @@ export default class WebSocket {
     private socket_session_id: string;
     private socket: Socket;
     public messages: any
-    private connections:any
+    private connections: any
     constructor(io: SocketServer) {
         this.io = io;
         this.ws_listeners = SessionListeners;
@@ -22,98 +22,116 @@ export default class WebSocket {
 
     start() {
         this.messages = {}
-            this.io.on(
-                this.ws_listeners.session.connection,
-                (socket: Socket) => {
-                    this.socket = socket;
-                    const { id } = this.socket;
-                    console.log(
-                        `👾 [websocket]: User ${id} connected on Websocket Server.`
-                    );
-                    /**
-                     * * Join Session
-                     */
-                    this.socket.on(
-                        this.ws_listeners.session.join_session,
-                        async ({ session_code, user }) => {
-                            if (!session_code || !user) {
-                                console.error("lacks information");
+        this.io.on(
+            this.ws_listeners.session.connection,
+            (socket: Socket) => {
+                this.socket = socket;
+                const { id } = this.socket;
+                console.log(
+                    `👾 [websocket]: User ${id} connected on Websocket Server.`
+                );
+                /**
+                 * * Join Session
+                 */
+                this.socket.on(
+                    this.ws_listeners.session.join_session,
+                    async ({ session_code, user }) => {
+                        if (!session_code || !user) {
+                            console.error("lacks information");
+                            return false;
+                        }
+
+                        console.log(user)
+                        try {
+                            const session: any =
+                                await this.sessionRepository.readOneBySessionCode(
+                                    session_code
+                                );
+                            if (!session) {
+                                console.error("not found");
                                 return false;
                             }
-                            
-                            console.log(user)
                             try {
-                                const session: any =
-                                    await this.sessionRepository.readOneBySessionCode(
-                                        session_code
-                                    );
-                                if (!session) {
-                                    console.error("not found");
-                                    return false;
-                                }
-                                try {
-                                    await this.sessionRepository.update(
-                                        session.id,
-                                        {
-                                            [`${user.type}_socket_id`]: id,
-                                        }
-                                    );
-                                    console.log("Updated session.");
-                                    console.log(session.session_id)
-                                    this.socket_session_id = session_code;
-                                    this.listenEvents(session_code, this.messages)
-                                } catch (update_session_error) {
-                                    console.error(update_session_error);
-                  
-                                    // TODO emit not updated session;
-                                }
-                                // * emit
-                            } catch (search_session_error) {
-                                console.error(search_session_error); // TODO emit not found session;
+                                await this.sessionRepository.update(
+                                    session.id,
+                                    {
+                                        [`${user.type}_socket_id`]: id,
+                                    }
+                                );
+                                console.log("Updated session.");
+                                console.log(session.session_id)
+                                this.socket_session_id = session_code;
+                                this.listenEvents(session_code, this.messages)
+                            } catch (update_session_error) {
+                                console.error(update_session_error);
+
+                                // TODO emit not updated session;
                             }
+                            // * emit
+                        } catch (search_session_error) {
+                            console.error(search_session_error); // TODO emit not found session;
                         }
-                    );
-                    this.socket.on('user-joined',async ({codigo}) => {
-                        const code = codigo.split('/')[3]
-                        if(!this.connections[code])
-                            this.connections[code] = []
-                        
-                        const session: any =
-                            await this.sessionRepository.readOneBySessionCode(
-                                code
-                            );
-                        if (!session) {
-                            console.error("not found");
-                            return false;
-                        }
-                        this.connections[code].push(id)
-                        if(this.connections[code].length >= 2){
-                            this.io.to(session.psicologo_socket_id).emit('start-cron',{paciente: session.paciente.nome})
-                            this.io.to(session.paciente_socket_id).emit('start-cron')
-                        }
-                    })
-                    this.socket.on('end-call', async ({code}) => {
-                        const session: any =
-                            await this.sessionRepository.readOneBySessionCode(
-                                code
-                            );
-                        if (!session) {
-                            console.error("not found");
-                            return false;
-                        }
-                        this.io.to(session.psicologo_socket_id).emit('end-call')
-                        this.io.to(session.paciente_socket_id).emit('end-call')
-                    })
-                    this.socket.on(this.ws_listeners.session.disconnect, () =>
+                    }
+                );
+                this.socket.on('user-joined', async ({ codigo }) => {
+                    const code = codigo.split('/')[3]
+                    if (!this.connections[code])
+                        this.connections[code] = []
+
+                    const session: any =
+                        await this.sessionRepository.readOneBySessionCode(
+                            code
+                        );
+                    await this.sessionRepository.update(
+                        session.id,
                         {
-                            this.messages = []
-                            this.connections = {}
-                            console.log(
-                                `🚪 [websocket]: User ${id} disconnected from Websocket Server.`
-                            )}
+                            session_out: Date.now(),
+                        }
                     );
+                    if (!session) {
+                        console.error("not found");
+                        return false;
+                    }
+                    this.connections[code].push(id)
+                    if (this.connections[code].length >= 2) {
+                        this.io.to(session.psicologo_socket_id).emit('start-cron', { paciente: session.paciente.nome })
+                        await this.sessionRepository.update(
+                            session.id,
+                            {
+                                paciente_in: Date.now(),
+                            }
+                        );
+                        this.io.to(session.paciente_socket_id).emit('start-cron')
+                    }
+                })
+                this.socket.on('end-call', async ({ code }) => {
+                    const session: any =
+                        await this.sessionRepository.readOneBySessionCode(
+                            code
+                        );
+                    await this.sessionRepository.update(
+                        session.id,
+                        {
+                            session_out: Date.now(),
+                        }
+                    );
+                    if (!session) {
+                        console.error("not found");
+                        return false;
+                    }
+                    this.io.to(session.psicologo_socket_id).emit('end-call')
+                    this.io.to(session.paciente_socket_id).emit('end-call')
+                })
+                this.socket.on(this.ws_listeners.session.disconnect, () => {
+                    this.messages = []
+                    this.connections = {}
+                    console.log(
+                        `🚪 [websocket]: User ${id} disconnected from Websocket Server.`
+                    )
                 }
-            );
+                );
+            }
+        );
 
     }
 
